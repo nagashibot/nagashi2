cbaf6b03d8db68185745ec40b59ccbc7bcd31711
nagashibot <80216329+nagashibot@users.noreply.github.com>
2021-05-01 06:59:34 -0300

    Add files via upload

---
+++ audio/falas/falas.js
@@ -0,0 +1,17 @@
+const falas = (prefix, pushname, time) => { 
+	return `
+	
+ *OLA* ${pushname}
+   *data e hora* ${time}
+   
+༆${prefix}ola *audio✿
+༆${prefix}pato ✿
+༆${prefix}bv audio ✿
+༆${prefix}tchau audio ✿
+༆${prefix}bem audio ✿
+༆${prefix}a   audio ✿
+༆${prefix}fdp audio ✿
+༆${prefix}onich ✿
+`
+}
+exports.falas = falas
